8ad63649a29a6ad96568de4deaca38c589ad15e2
jgp <jgp@jgp.net>
2018-04-22 10:42:35 -0400

    New example without the short name

---
+++ src/main/java/net/jgp/books/sparkWithJava/ch09/lab101/photoDatasourceNoShortName/PhotoMetadataIngestionNoShortNameApp.java
@@ -0,0 +1,47 @@
+package net.jgp.books.sparkWithJava.ch09.lab101.photoDatasourceNoShortName;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * Ingest metadata from a directory containing photos, make them available as
+ * EXIF.
+ * 
+ * @author jgp
+ */
+public class PhotoMetadataIngestionNoShortNameApp {
+  public static void main(String[] args) {
+    PhotoMetadataIngestionNoShortNameApp app = new PhotoMetadataIngestionNoShortNameApp();
+    app.start();
+  }
+
+  /**
+   * Starts the application
+   * 
+   * @return <code>true</code> if all is ok.
+   */
+  private boolean start() {
+    // Get a session
+    SparkSession spark = SparkSession.builder()
+        .appName("EXIF to Dataset")
+        .master("local").getOrCreate();
+
+    // Import directory
+    String importDirectory = "data";
+
+    // read the data
+    Dataset<Row> df = spark.read()
+        .format("net.jgp.books.sparkWithJava.ch09.x.ds.exif.ExifDirectoryDataSourceShortnameAdvertiser")
+        .option("recursive", "true")
+        .option("limit", "100000")
+        .option("extensions", "jpg,jpeg")
+        .load(importDirectory);
+
+    System.out.println("I have imported " + df.count() + " photos.");
+    df.printSchema();
+    df.show(5);
+
+    return true;
+  }
+}
