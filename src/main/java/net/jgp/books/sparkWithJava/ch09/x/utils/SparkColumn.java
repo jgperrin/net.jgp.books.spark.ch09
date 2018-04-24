66e3ff9e24979da7154adbedf7828ca8bd6a7003
jgp <jgp@jgp.net>
2018-04-24 04:56:55 -0400

    Comments

--- src/main/java/net/jgp/books/sparkWithJava/ch09/x/utils/SparkColumn.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch09/x/utils/SparkColumn.java
@@ -5,12 +5,27 @@ import java.lang.annotation.RetentionPolicy;
 
 import org.apache.spark.sql.types.DataType;
 
+/**
+ * Simple annotation to extend and ease the Javabean metadata when converting to
+ * a Spark column in a dataframe.
+ * 
+ * @author jgp
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SparkColumn {
 
+  /**
+   * The name of the column can be overriden.
+   */
   String name() default "";
 
+  /**
+   * Forces the data type of the column
+   */
   Class<? extends DataType> type() default DataType.class;
 
+  /**
+   * Forces the required/nullable property
+   */
   boolean nullable() default true;
 }
